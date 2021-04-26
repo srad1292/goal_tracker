742ebcc26dc3d61ffe30db4ed1405f051330bb08
Sam Radford <smr1292@yahoo.com>
2021-04-26 17:20:11 -0400

    Added schema to database connection

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -17,11 +17,10 @@ func connectToDatabase() *pgxpool.Pool {
 	db_host := os.Getenv("DB_HOST")
 	db_port := os.Getenv("DB_PORT")
 	db_name := os.Getenv("DB_NAME")
-	// db_schema := os.Getenv("DB_SCHEMA")
+	db_schema := os.Getenv("DB_SCHEMA")
 	db_conn := os.Getenv("DB_MAX_CONNECTIONS")
 
-	var dbURL string = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?pool_max_conns=%s", db_user, db_password, db_host, db_port, db_name, db_conn)
-	// url = postgres://jack:secret@pg.example.com:5432/mydb?sslmode=verify-ca&pool_max_conns=10
+	var dbURL string = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?pool_max_conns=%s&search_path=%s", db_user, db_password, db_host, db_port, db_name, db_conn, db_schema)
 	dbpool, err := pgxpool.Connect(context.Background(), dbURL)
 
 	if err != nil {
